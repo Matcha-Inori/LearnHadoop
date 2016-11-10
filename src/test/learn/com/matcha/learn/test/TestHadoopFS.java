9237b78473ccfb98b78c1c275f312db34285dc39
Matcha-Inori <513034940@qq.com>
2016-11-10 23:16:43 +0800

    增加一些hdfs的测试，尝试上传和读取文件

---
+++ src/test/learn/com/matcha/learn/test/TestHadoopFS.java
@@ -0,0 +1,108 @@
+package com.matcha.learn.test;
+
+import io.netty.buffer.ByteBuf;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.permission.FsAction;
+import org.apache.hadoop.fs.permission.FsPermission;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+
+/**
+ * Created by Matcha on 2016/11/9.
+ */
+@RunWith(Parameterized.class)
+public class TestHadoopFS
+{
+    private FileSystem fileSystem;
+
+    public TestHadoopFS(String fsURI)
+    {
+        try
+        {
+            URI uri = URI.create(fsURI);
+            Configuration configuration = new Configuration();
+            fileSystem = FileSystem.get(uri, configuration);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testLoadFile() throws IOException, URISyntaxException
+    {
+        Path newFilePath = new Path("/matcha/test/fistFile.txt");
+        URI firstLocalFileURI = new File("").toURI().resolve("src/main/resources/output/firstLocalFile.txt");
+        try(
+                FSDataInputStream fsDataInputStream = fileSystem.open(newFilePath);
+                ReadableByteChannel readableByteChannel = Channels.newChannel(fsDataInputStream);
+                RandomAccessFile randomAccessFile = new RandomAccessFile(new File(firstLocalFileURI), "rw");
+                FileChannel fileChannel = randomAccessFile.getChannel()
+        )
+        {
+            fileChannel.transferFrom(readableByteChannel, 0, fsDataInputStream.available());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testCreateFile() throws IOException, URISyntaxException
+    {
+        //这个地方的链接其实涉及到hdfs的登录，不过先暂时关闭hdfs的权限认证吧
+        Path newFilePath = new Path("/matcha/test/fistFile.txt");
+        FsPermission fsPermission = new FsPermission(FsAction.ALL, FsAction.READ, FsAction.READ);
+        URL localFileURL = Thread.currentThread().getContextClassLoader().getResource("data/testFile.txt");
+        try(
+                FSDataOutputStream fsDataOutputStream = fileSystem.create(newFilePath);
+                WritableByteChannel writableByteChannel = Channels.newChannel(fsDataOutputStream);
+                RandomAccessFile randomAccessFile = new RandomAccessFile(new File(localFileURL.toURI()), "r");
+                FileChannel fileChannel = randomAccessFile.getChannel();
+        )
+        {
+            fileChannel.transferTo(0, fileChannel.size(), writableByteChannel);
+            fileSystem.setPermission(newFilePath, fsPermission);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw e;
+        }
+        catch (URISyntaxException e)
+        {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] provideParams()
+    {
+        return new Object[][]{
+                {
+                        "hdfs://192.168.56.101:9000/"
+                }
+        };
+    }
+}
---
+++ src/test/learn/com/matcha/learn/test/TestJUnit4.java
@@ -0,0 +1,124 @@
+package com.matcha.learn.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.UUID;
+
+/**
+ * Created by Matcha on 2016/11/9.
+ */
+@RunWith(Parameterized.class)
+public class TestJUnit4
+{
+    private static String str = "default";
+    private static ThreadLocal<UUID> threadId = new ThreadLocal<UUID>()
+    {
+        @Override
+        protected UUID initialValue()
+        {
+            return UUID.randomUUID();
+        }
+    };
+
+    private Person person1;
+    private Person person2;
+    private Person[] persons;
+
+    public TestJUnit4(Person person1, Person person2, Person[] persons)
+    {
+        this.person1 = person1;
+        this.person2 = person2;
+        this.persons = persons;
+    }
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        System.out.println("Thread - " + threadId.get() + " start! and str is " + str);
+        str = "abcdefg";
+    }
+
+    @AfterClass
+    public static void afterClass()
+    {
+        System.out.println("Thread - " + threadId.get() + " finish! and str is " + str);
+    }
+
+    @Test
+    public void showPerson()
+    {
+        System.out.println("Thread - " + threadId.get() + " first person is " + person1);
+        System.out.println("Thread - " + threadId.get() + " second person is " + person2);
+        for(Person person : persons)
+            System.out.println("Thread - " + threadId.get() + " persons - " + person);
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] provideParams()
+    {
+        return new Object[][]{
+                {
+                        new Person("AAA", 1),
+                        new Person("BBB", 2),
+                        new Person[]{
+                                new Person("CCC", 3)
+                        }
+                },
+                {
+                        new Person("DDD", 4),
+                        new Person("EEE", 5),
+                        new Person[]{
+                                new Person("FFF", 6),
+                                new Person("GGG", 7)
+                        }
+                }
+        };
+    }
+}
+
+class Person
+{
+    private String name;
+    private int age;
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
+
